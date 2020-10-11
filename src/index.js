dd04100b69957c12235a2a65b592a0020698fbf5
necatiozmen <necati.ozmen.9@gmail.com>
2020-10-11 10:10:36 +0300

    add build config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from './App';
 import sum from './sum.js';
 
-console.log(sum(2,5))
 
+console.log(sum(2, 5))
+console.log(process.env.NODE_ENV)
 const title = "React with webpack and babel";
+
 
 ReactDOM.render(<App title={title} />, document.getElementById("app"));
 
